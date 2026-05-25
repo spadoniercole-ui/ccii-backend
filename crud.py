17cff5d3e04abb880bc7b65fc12b838cb1b1c218
spadoniercole-ui <spadoni.ercole@gmail.com>
2026-05-25 11:17:51 +0200

    Create crud.py

---
+++ crud.py
@@ -0,0 +1,19 @@
+# crud.py
+from sqlalchemy.orm import Session
+from models import User
+from utils import get_password_hash # Importa la funzione qui
+
+def create_user(db: Session, email: str, password: str, role: str = "user", is_superuser: bool = False):
+    # Usi la funzione importata da utils
+    hashed_pw = get_password_hash(password)
+    
+    db_user = User(
+        email=email, 
+        hashed_password=hashed_pw, 
+        role=role, 
+        is_superuser=is_superuser
+    )
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
