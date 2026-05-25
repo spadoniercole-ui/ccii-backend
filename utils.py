0bb7df550a455c02af0aa5f2a2e19f917b22ec31
spadoniercole-ui <spadoni.ercole@gmail.com>
2026-05-25 10:39:03 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,6 +1,17 @@
-from passlib.context import CryptContext
+# crud.py
+from sqlalchemy.orm import Session
+from models import User
+from utils import get_password_hash
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-
-def get_password_hash(password):
-    return pwd_context.hash(password)
+def create_user(db: Session, email: str, password: str, role: str = "user", is_superuser: bool = False):
+    hashed_pw = get_password_hash(password)
+    db_user = User(
+        email=email, 
+        hashed_password=hashed_pw, 
+        role=role, 
+        is_superuser=is_superuser
+    )
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
