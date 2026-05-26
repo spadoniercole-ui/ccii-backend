b22522f2d48eed84431a8a9b5b80b6fb24c373e4
spadoniercole-ui <spadoni.ercole@gmail.com>
2026-05-26 21:06:44 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -3,6 +3,20 @@ import bcrypt
 if not hasattr(bcrypt, "__about__"):
     bcrypt.__about__ = type("About", (object,), {"__version__": bcrypt.__version__})
 
+# utils.py originale con Argon2
+from argon2 import PasswordHasher
+from argon2.exceptions import VerifyMismatchError
+
+ph = PasswordHasher()
+
+def get_password_hash(password: str) -> str:
+    return ph.hash(password)
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    try:
+        return ph.verify(hashed_password, plain_password)
+    except VerifyMismatchError:
+        return False
 # utils.py
 from argon2 import PasswordHasher
 from argon2.exceptions import VerifyMismatchError
